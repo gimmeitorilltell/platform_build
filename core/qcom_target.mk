fea7c0dcabcca03558417e49d900eaf298068c5b
Ethan Chen <intervigil@gmail.com>
2014-12-15 18:44:31 -0800

    build: Clean up QCOM flag definitions
    Change-Id: I66bca2db83260ccd65b82e540ee9f7961f00b030
    
    Conflicts:
    	core/qcom_target.mk

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -7,22 +7,19 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-    TARGET_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-    TARGET_GLOBAL_CPPFLAGS += -DQCOM_HARDWARE
+    qcom_flags := -DQCOM_HARDWARE
+    qcom_flags += -DQCOM_BSP
 
     TARGET_USES_QCOM_BSP := true
-    TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
-    TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
-
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
     # Enable DirectTrack for legacy targets
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
-    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-        TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
-        TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
-    endif
+        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+            qcom_flags += -DQCOM_DIRECTTRACK
+        endif
         # Enable legacy graphics functions
+
         qcom_flags += -DQCOM_BSP_LEGACY
      endif
  
