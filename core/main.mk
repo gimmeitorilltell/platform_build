6c7d910836986a1c2d41df946fa1f1bf9308aec8
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2016-01-12 19:39:44 -0500

    Don't error out if we want to use Oracle Java 8
    Change-Id: Ic5719d0e60fb9a3e29052931195a8d1f56094d4f
    Signed-off-by: gimmeitorilltell <gimmeitorilltell@yahoo.com>

--- core/main.mk
+++ core/main.mk
@@ -175,7 +175,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/initializing.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 # Check for the current JDK.
@@ -188,7 +187,7 @@ ifeq ($(shell echo '$(java_version_str)' | grep '1.8'),)
 requires_openjdk := true
 endif
 endif
-
+endif
 # Check for the current jdk
 ifeq ($(requires_openjdk), true)
 # The user asked for java7 openjdk, so check that the host
@@ -198,7 +197,6 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -209,7 +207,6 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -225,7 +222,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 
